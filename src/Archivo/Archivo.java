22468de1d1c07e24571e5dfd5a3edfc933d8635b
Fede <fedeboccazzi@gmail.com>
2024-10-21 19:03:40 -0300

    defini nombre de metodos y que deberian hacer para procesar los archivos

--- src/Archivo/Archivo.java
+++ src/Archivo/Archivo.java
@@ -8,8 +8,20 @@ public class Archivo {
 
     }
 
-    public <T> Tabla importar(String path, char sep, Boolean header) {
-        Tabla tablaImportada = new Tabla<T>();
+    public Tabla importar(String path, char sep, Boolean header) {
+        Tabla tablaImportada = new Tabla();
         return tablaImportada;
+    }
+
+    public void parseCSV() {
+        //crear metodo para que recorra datos de un archivo CSV
+    }
+
+    public void cargarDatoColumna() {
+        // crear metodo para carga dato de parseCSV y lo cargue en una Columna
+    }
+
+    public void extraerDatoColumna() {
+        // crear metodo para extraer Dato de Columna y lo envie para exportar a un archivo
     }
 }
