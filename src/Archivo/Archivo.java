2f793113627c0f8edbffa0fd2557a78e7edbb082
Fede <fedeboccazzi@gmail.com>
2024-11-09 21:30:26 -0300

    agrego javadoc a Archivo y GroupBy

--- src/Archivo/Archivo.java
+++ src/Archivo/Archivo.java
@@ -14,32 +14,58 @@ import java.util.List;
 
 import Columna.Columna;
 
+/**
+ * La clase Archivo proporciona métodos para exportar e importar datos de una
+ * tabla a un archivo CSV, utilizando diferentes configuraciones para el
+ * separador y el encabezado.
+ */
 public class Archivo {
-    
-    //Metodos publicos
+
+    /**
+     * Exporta una tabla a un archivo CSV con un separador por defecto (coma) y con encabezado.
+     *
+     * @param tabla         la tabla a exportar.
+     * @param nombreArchivo el nombre del archivo donde se exportará la tabla.
+     * @throws ArchivoException si ocurre algún error al exportar los datos.
+     */
     public static void exportar(Tabla tabla, String nombreArchivo) throws ArchivoException {
-        // Exporta una tabla a un archivo con valores por defecto para sep y header
         exportar(tabla, nombreArchivo, ",", true); // Valores por defecto para `sep` y `header`
     }
 
+    /**
+     * Exporta una tabla a un archivo CSV con encabezado y un separador específico.
+     *
+     * @param tabla         la tabla a exportar.
+     * @param nombreArchivo el nombre del archivo donde se exportará la tabla.
+     * @param sep           el separador que se usará en el archivo CSV.
+     * @throws ArchivoException si ocurre algún error al exportar los datos.
+     */
     public static void exportar(Tabla tabla, String nombreArchivo, String sep) throws ArchivoException {
-        // Exporta una tabla a un archivo con valores por defecto para header
         exportar(tabla, nombreArchivo, sep, true); // Valor por defecto para `header`
     }
 
+    /**
+     * Exporta una tabla a un archivo CSV con opciones específicas para el separador y si
+     * debe incluir encabezado.
+     *
+     * @param tabla         la tabla a exportar.
+     * @param nombreArchivo el nombre del archivo donde se exportará la tabla.
+     * @param sep           el separador que se usará en el archivo CSV.
+     * @param header        si es true, incluye encabezados de columna en el archivo.
+     * @throws ArchivoException si ocurre algún error al exportar los datos.
+     */
     public static void exportar(Tabla tabla, String nombreArchivo, String sep, Boolean header) throws ArchivoException {
-        // Exporta una tabla a un archivo aclarando cual separador usar y si tiene encabezados las columnas
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(nombreArchivo))) {
             if (header) {
                 List<String> encabezados = new ArrayList<>();
                 for (Columna<?, ?> c : tabla.getColumnas()) {
                     encabezados.add(c.getetiqueta().toString());
-                } 
+                }
                 String encabezado = String.join(sep, encabezados);
                 writer.write(encabezado);
                 writer.newLine();
             }
-            
+
             int numFilas = tabla.getColumnas().get(0).getDatos().size();
             for (int i = 0; i < numFilas; i++) {
                 List<String> fila = new ArrayList<>();
@@ -57,23 +83,33 @@ public class Archivo {
         } catch (IOException e) {
             throw new ArchivoException("Error al exportar los datos a " + nombreArchivo + e);
         } catch (IndexOutOfBoundsException e) {
-            throw new ArchivoException("La tabla a exportar esta vacia");
+            throw new ArchivoException("La tabla a exportar está vacía");
         }
     }
 
+    /**
+     * Importa datos desde un archivo CSV y los carga en una tabla, utilizando un
+     * separador específico y considerando si incluye encabezado.
+     *
+     * @param nombreArchivo el nombre del archivo CSV desde el cual importar datos.
+     * @param sep           el separador usado en el archivo CSV.
+     * @param header        si es true, el archivo incluye encabezados de columna.
+     * @return una tabla con los datos importados.
+     * @throws IOException si ocurre algún error al leer el archivo.
+     */
     public static Tabla importar(String nombreArchivo, String sep, Boolean header) throws IOException {
         try {
             Tabla tablaImportada = new Tabla();
             List<Object[]> datos = parseCSV(nombreArchivo, sep, header);
             List<String> etiquetasColumnas = new ArrayList<>();
-    
+
             if (header && !datos.isEmpty()) {
                 for (Object etiqueta : datos.get(0)) {
                     etiquetasColumnas.add((String) etiqueta);
                 }
                 datos.remove(0);
             }
-    
+
             List<Object[]> columnas = filasAColumnas(datos);
             for (int i = 0; i < columnas.size(); i++) {
                 String etiquetaColumna;
@@ -92,21 +128,25 @@ public class Archivo {
         return null;
     }
 
-    //Metodos privados
+    /**
+     * Convierte una lista de datos organizada en filas en una lista organizada en columnas.
+     *
+     * @param datos los datos en formato de filas.
+     * @return los datos reorganizados en formato de columnas.
+     */
     private static List<Object[]> filasAColumnas(List<Object[]> datos) {
-        //Convierte los datos de filas para utilizarlos como columnas.
         List<Object[]> datosColumna = new ArrayList<>();
-        
+
         if (datos.isEmpty()) {
             return datosColumna;
         }
 
         int numColumnas = datos.get(0).length;
-        
+
         for (int i = 0; i < numColumnas; i++) {
             datosColumna.add(new Object[datos.size()]);
         }
-    
+
         for (int i = 0; i < datos.size(); i++) {
             Object[] fila = datos.get(i);
             for (int j = 0; j < fila.length; j++) {
@@ -116,17 +156,24 @@ public class Archivo {
         return datosColumna;
     }
 
+    /**
+     * Lee un archivo CSV y convierte sus líneas en una lista de arreglos de
+     * objetos.
+     *
+     * @param nombreArchivo el nombre del archivo CSV.
+     * @param sep           el separador usado en el archivo CSV.
+     * @param header        si es true, el archivo incluye encabezados de columna.
+     * @return una lista de arreglos de objetos con los datos del archivo CSV.
+     */
     private static List<Object[]> parseCSV(String nombreArchivo, String sep, Boolean header) {
-        // Recorre un archivo de texto para extraer los datos 
         List<Object[]> datos = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(nombreArchivo))) {
             String linea;
-    
-            // Leer el resto de las líneas
+
             while ((linea = bufferedReader.readLine()) != null) {
-                String[] valores = linea.split(sep,-1);
+                String[] valores = linea.split(sep, -1);
                 Object[] valoresConvertidos = new Object[valores.length];
-    
+
                 for (int i = 0; i < valores.length; i++) {
                     if (valores[i].isEmpty()) {
                         valoresConvertidos[i] = null;
@@ -141,12 +188,17 @@ public class Archivo {
         }
         return datos;
     }
-    
+
+    /**
+     * Detecta y convierte el tipo de dato adecuado para un valor de texto.
+     *
+     * @param valor el valor en formato de texto.
+     * @return el valor convertido en su tipo de dato correspondiente.
+     */
     private static Object detectarTipo(String valor) {
-        // Método para detectar el tipo de dato de cada valor
         if (valor.equalsIgnoreCase("true") || valor.equalsIgnoreCase("false")) {
             return Boolean.parseBoolean(valor);
-        } 
+        }
         try {
             return Integer.parseInt(valor);
         } catch (NumberFormatException e1) {
--- src/groupby/GroupBy.java
+++ src/groupby/GroupBy.java
@@ -1,20 +1,28 @@
 package groupby;
 
-import Filtro.Filtro;
 import Operaciones.OperacionesColumna;
 import Tabla.Tabla;
-import java.util.Map;
 import java.util.Set;
-
 import Columna.Columna;
-
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * La clase GroupBy proporciona métodos para agrupar y realizar operaciones
+ * en una columna específica de una tabla.
+ */
 public class GroupBy {
 
+    /**
+     * Agrupa una tabla en función de los valores únicos de una columna específica.
+     *
+     * @param <T>     el tipo de datos en la columna a agrupar.
+     * @param tabla   la tabla que se desea agrupar.
+     * @param columna el nombre de la columna por la cual se desea agrupar.
+     * @return un objeto ResultadoGroupBy que contiene las tablas agrupadas
+     *         y la lista de valores únicos en la columna.
+     */
     public static <T> ResultadoGroupBy<T> groupBy(Tabla tabla, String columna) {
         List<Tabla> tablas = new ArrayList<>();
         Columna datosColumna = tabla.obtenerColumnaPorEtiqueta(columna);
@@ -31,6 +39,15 @@ public class GroupBy {
         return new ResultadoGroupBy<>(tablas, listaUnica);
     }
 
+    /**
+     * Aplica una operación a una columna específica de cada grupo en una lista de tablas agrupadas.
+     *
+     * @param <T>             el tipo de datos en la columna sobre la cual se desea realizar la operación.
+     * @param tablasAgrupadas el resultado de una operación de agrupamiento (groupBy).
+     * @param columna         el nombre de la columna sobre la cual se aplicará la operación.
+     * @param operacion       el tipo de operación a aplicar (sumar, contar, desvio, maximo, minimo, promediar, varianza).
+     * @return una nueva tabla que contiene los valores únicos de la columna agrupada y el resultado de la operación aplicada.
+     */
     public static <T> Tabla aplicarAColumna(ResultadoGroupBy tablasAgrupadas, String columna, String operacion) {
         Tabla tablaAgrupada = new Tabla();
         List<Tabla> tablas = tablasAgrupadas.getTablas();
@@ -39,9 +56,10 @@ public class GroupBy {
         Columna datosColumna;
         OperacionesColumna resultado;
 
+        // Agrega la columna de valores únicos a la nueva tabla
         tablaAgrupada.agregarColumna(valoresUnicos);
 
-        for (int i= 0; i < valoresUnicos.size(); i++) {
+        for (int i = 0; i < valoresUnicos.size(); i++) {
             datosColumna = tablas.get(i).obtenerColumnaPorEtiqueta(columna);
             resultado = new OperacionesColumna(datosColumna.getDatos());
             switch (operacion.toLowerCase()) {
@@ -69,9 +87,9 @@ public class GroupBy {
                 default:
                     break;
             }
-            
         }
 
+        // Agrega los resultados de la operación a la nueva tabla
         tablaAgrupada.agregarColumna(valoresOperacion);
 
         return tablaAgrupada;
@@ -80,20 +98,38 @@ public class GroupBy {
     /**
      * Clase auxiliar para almacenar el resultado del método groupBy.
      * Contiene una lista de tablas y una lista de valores únicos.
+     *
+     * @param <T> el tipo de datos en la lista de valores únicos.
      */
     public static class ResultadoGroupBy<T> {
         private List<Tabla> tablas;
         private List<T> valoresUnicos;
 
+        /**
+         * Crea una nueva instancia de ResultadoGroupBy.
+         *
+         * @param tablas        la lista de tablas agrupadas por valores únicos.
+         * @param valoresUnicos la lista de valores únicos que se usaron para agrupar las tablas.
+         */
         public ResultadoGroupBy(List<Tabla> tablas, List<T> valoresUnicos) {
             this.tablas = tablas;
             this.valoresUnicos = valoresUnicos;
         }
 
+        /**
+         * Obtiene la lista de tablas agrupadas.
+         *
+         * @return la lista de tablas agrupadas.
+         */
         public List<Tabla> getTablas() {
             return tablas;
         }
 
+        /**
+         * Obtiene la lista de valores únicos que se usaron para agrupar las tablas.
+         *
+         * @return la lista de valores únicos.
+         */
         public List<T> getValoresUnicos() {
             return valoresUnicos;
         }
