bb56907a0c423f4b6e8845d00330a5d1cc832923
Fede <fedeboccazzi@gmail.com>
2024-11-10 20:51:59 -0300

    Javadoc a Operaciones y OperacionesColumna

--- src/Operaciones/Operaciones.java
+++ src/Operaciones/Operaciones.java
@@ -1,12 +1,52 @@
 package Operaciones;
 
-public abstract class Operaciones <T> {
+/**
+ * Clase abstracta que define un conjunto de operaciones estadísticas 
+ * que se pueden realizar sobre una colección de datos.
+ * @param <T> El tipo de dato sobre el que se realizarán las operaciones.
+ */
+public abstract class Operaciones<T> {
 
+    /**
+     * Calcula la suma de los elementos en la colección de datos.
+     * @return El resultado de la suma como un valor de tipo T.
+     */
     public abstract T sumar();
+
+    /**
+     * Cuenta el número de elementos en la colección de datos.
+     * @return El número de elementos como un valor de tipo T.
+     */
     public abstract T contar();
+
+    /**
+     * Calcula el promedio de los elementos en la colección de datos.
+     * @return El promedio de los elementos como un valor de tipo T.
+     */
     public abstract T promediar();
+
+    /**
+     * Encuentra el valor máximo en la colección de datos.
+     * @return El valor máximo como un valor de tipo T.
+     */
     public abstract T maximo();
+
+    /**
+     * Encuentra el valor mínimo en la colección de datos.
+     * @return El valor mínimo como un valor de tipo T.
+     */
     public abstract T minimo();
+
+    /**
+     * Calcula la varianza de los elementos en la colección de datos.
+     * @return La varianza como un valor de tipo T.
+     */
     public abstract T varianza();
+
+    /**
+     * Calcula el desvío estándar de los elementos en la colección de datos.
+     * @return El desvío estándar como un valor de tipo T.
+     */
     public abstract T desvio();
 }
+
--- src/Operaciones/OperacionesColumna.java
+++ src/Operaciones/OperacionesColumna.java
@@ -1,14 +1,27 @@
 package Operaciones;
 import java.util.List;
 
+/**
+ * Clase que realiza operaciones estadísticas sobre una lista de valores numéricos.
+ * Extiende la clase abstracta Operaciones para proporcionar implementaciones de suma, conteo,
+ * promedio, máximo, mínimo, varianza y desvío estándar.
+ */
 public class OperacionesColumna extends Operaciones<Double> {
 
     private List<Number> datos;
 
+    /**
+     * Constructor que inicializa la lista de datos sobre la que se realizarán las operaciones.
+     * @param datos La lista de valores numéricos.
+     */
     public OperacionesColumna(List<Number> datos) {
         this.datos = datos;
     }
 
+    /**
+     * Calcula la suma de todos los elementos en la lista de datos.
+     * @return La suma total como un valor de tipo Double.
+     */
     @Override
     public Double sumar() {
         double suma = 0;
@@ -20,16 +33,28 @@ public class OperacionesColumna extends Operaciones<Double> {
         return suma;
     }
 
+    /**
+     * Cuenta el número de elementos en la lista de datos.
+     * @return El número de elementos como un valor de tipo Double.
+     */
     @Override
     public Double contar() {
         return (double) datos.size();
     }
 
+    /**
+     * Calcula el promedio de los elementos en la lista de datos.
+     * @return El promedio de los elementos como un valor de tipo Double.
+     */
     @Override
     public Double promediar() {
         return sumar() / contar();
     }
 
+    /**
+     * Encuentra el valor máximo en la lista de datos.
+     * @return El valor máximo como un valor de tipo Double.
+     */
     @Override
     public Double maximo() {
         double max = Double.NEGATIVE_INFINITY;
@@ -41,6 +66,10 @@ public class OperacionesColumna extends Operaciones<Double> {
         return max;
     }
 
+    /**
+     * Encuentra el valor mínimo en la lista de datos.
+     * @return El valor mínimo como un valor de tipo Double.
+     */
     @Override
     public Double minimo() {
         double min = Double.POSITIVE_INFINITY;
@@ -52,6 +81,10 @@ public class OperacionesColumna extends Operaciones<Double> {
         return min;
     }
 
+    /**
+     * Calcula la varianza de los elementos en la lista de datos.
+     * @return La varianza como un valor de tipo Double.
+     */
     @Override
     public Double varianza() {
         double promedio = promediar();
@@ -65,10 +98,12 @@ public class OperacionesColumna extends Operaciones<Double> {
         return sumaCuadrados / contar();
     }
 
+    /**
+     * Calcula el desvío estándar de los elementos en la lista de datos.
+     * @return El desvío estándar como un valor de tipo Double.
+     */
     @Override
     public Double desvio() {
         return Math.sqrt(varianza());
     }
-
-    }
-
+}
