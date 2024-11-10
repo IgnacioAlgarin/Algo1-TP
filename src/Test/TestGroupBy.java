86068050e01d100858aaae9cf14f7d504e5baa24
Fede <fedeboccazzi@gmail.com>
2024-11-09 21:25:11 -0300

    manejo de errores Test groupby

--- src/Test/TestGroupBy.java
+++ src/Test/TestGroupBy.java
@@ -11,22 +11,23 @@ import groupby.GroupBy;
 
 public class TestGroupBy {
     public static void main(String[] args) throws Exception  {
-        System.out.println("Inicio test groupBy");
+        System.out.println("Inicio test groupBy\n");
         Tabla tablaAgrupar = new Tabla();
         tablaAgrupar = Archivo.importar("src/datos_GroupBy.csv",",", false);
 
         Tabla tabla = GroupBy.aplicarAColumna(GroupBy.groupBy(tablaAgrupar, "Columna_1"), "Columna_0", "Contar");
         tabla.visualizar();
 
-        // List<Tabla> tablas = GroupBy.groupBy(tablaAgrupar, "Columna_1");
-        // Columna col = GroupBy.aplicarAColumna(tablas, "Columna_0");
-        
-        // for (Tabla tabla: tablas) {
-        //     int i = 1;
-        //     System.out.println("Tabla " + i++);
-        //     tabla.visualizar();
-        // }
-        // System.out.println("Fin test");
+        System.out.println("Test con columna booleana");
+        tabla = GroupBy.aplicarAColumna(GroupBy.groupBy(tablaAgrupar, "Columna_2"), "Columna_0", "Contar");
+        tabla.visualizar();
+
+        System.out.println("Test de operaciones pero en columna NO numerica");
+        tabla = GroupBy.aplicarAColumna(GroupBy.groupBy(tablaAgrupar, "Columna_1"), "Columna_2", "sumar");
+        tabla.visualizar();
+
+        System.out.println("Fin de test\n");
+
     }
 
 }
